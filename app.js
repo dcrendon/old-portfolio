2cf88e72a29903137565e2ad89242f469cae6de7
Daniel Rendon <daniel.rendonfc@gmail.com>
2017-02-23 14:50:30 -0600

    contact link in nav works

--- app.js
+++ app.js
@@ -2,4 +2,22 @@ $(document).ready(function() {
   $('.tooltipped').tooltip({delay: 50});
   $(".button-collapse").sideNav();
   $('#bod').animate({opacity: 0}, 0).css({'background-image': 'url("vape.jpg") no-repeat center center fixed'}).animate({opacity: 1}, 2500);
+
+
+  $(".nav-contact").click(function() {
+    // add neon for first time
+    $(".foot-icon").addClass("play");
+    // remove neon
+    setTimeout(function() {
+      $(".foot-icon").removeClass("play");
+    }, 2000);
+    // readd neon
+    // setTimeout(function() {
+    //   $(".foot-icon").addClass("neon");
+    // }, 2100);
+    // remove neon for last time
+    // setTimeout(function() {
+    //   $(".foot-icon").addClass("neon");
+    // }, 4100);
+  });
 });
