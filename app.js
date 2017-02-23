7ce12196b4e9655702300e528b56a23a8c66af46
Daniel Rendon <daniel.rendonfc@gmail.com>
2017-02-23 17:32:05 -0600

    work tab updates

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
 $(document).ready(function() {
+
+
   $('.tooltipped').tooltip({delay: 50});
   $(".button-collapse").sideNav();
   $('#bod').animate({opacity: 0}, 0).css({'background-image': 'url("vape.jpg") no-repeat center center fixed'}).animate({opacity: 1}, 2500);
 
 
   $(".nav-contact").click(function() {
-    // add neon for first time
+    // add neon
     $(".foot-icon").addClass("play");
     // remove neon
     setTimeout(function() {
       $(".foot-icon").removeClass("play");
     }, 2000);
-    // readd neon
-    // setTimeout(function() {
-    //   $(".foot-icon").addClass("neon");
-    // }, 2100);
-    // remove neon for last time
-    // setTimeout(function() {
-    //   $(".foot-icon").addClass("neon");
-    // }, 4100);
   });
+
+  var cards = false;
+  console.log(cards);
+  $(".nav-work").click(function () {
+    console.log(cards);
+    if (cards) {
+      cards = false;
+      // remove existing classes
+      $(".work-left").removeClass("animated bounceInLeft");
+      $(".work-right").removeClass("animated bounceInRight");
+      // make cards leave
+      $(".work-left").addClass("animated bounceOutLeft");
+      $(".work-right").addClass("animated bounceOutRight");
+      $('.work-right, .work-left').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+        $(".work-right, .work-left").addClass("hidden");
+      });
+      console.log(cards);
+    }else {
+      // remove existing classes
+      $(".work-right, .work-left").removeClass("hidden");
+      $(".work-left").removeClass("animated bounceOutLeft");
+      $(".work-right").removeClass("animated bounceOutRight");
+      // make cards enter
+      $(".work-left").addClass("animated bounceInLeft");
+      $(".work-right").addClass("animated bounceInRight");
+      $('.work-right, .work-left').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+        $(".work-right, .work-left").removeClass("hidden");
+      });
+      $(".work-right, .work-left").removeClass("hidden");
+      cards = true;
+      console.log(cards);
+    };
+
+
+
+  });
+
+
 });
