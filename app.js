1e7a9e3f5d357c31cb8b67bdd0a30a8377142c71
Daniel Rendon <daniel.rendonfc@gmail.com>
2017-02-23 12:50:24 -0600

    footer update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
   $('.tooltipped').tooltip({delay: 50});
+  $(".button-collapse").sideNav();
   $('#bod').animate({opacity: 0}, 0).css({'background-image': 'url("vape.jpg") no-repeat center center fixed'}).animate({opacity: 1}, 2500);
 });
