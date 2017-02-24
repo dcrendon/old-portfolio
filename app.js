ff291a598f7a72f8850b1ec37a5bbb4d147e129f
Daniel Rendon <daniel.rendonfc@gmail.com>
2017-02-24 10:18:08 -0600

    sidenav closes on click

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
 
-
+  // initialize
   $('.tooltipped').tooltip({delay: 50});
-  $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav({
+    menuWidth: 300, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+  });
   $('#bod').animate({opacity: 0}, 0).css({'background-image': 'url("vape.jpg") no-repeat center center fixed'}).animate({opacity: 1}, 2500);
 
 
